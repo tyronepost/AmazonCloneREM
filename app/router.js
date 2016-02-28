1a9d421c012585b676da72880413dcfa70601197
Tyrone Post <tyrone.post.a@gmail.com>
2016-02-28 14:55:27 -0500

    adding nested route for todays deals navigation

--- app/router.js
+++ app/router.js
@@ -7,7 +7,8 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('index', {path: '/'} ,function() {
-    this.route('books' );
+    this.route('books');
+    this.route('deals');
   });
 });
 
