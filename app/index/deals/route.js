af58fe318559915b5f09dbdba5af0c70ae0aff64
Tyrone Post <tyrone.post.a@gmail.com>
2016-03-02 14:43:56 -0500

    Added deals model.  Updated deals template to pull carousel images from model.

--- app/index/deals/route.js
+++ app/index/deals/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
+var deals = [
+  { image: 'assets/images/xboxone.png', active: true },
+  { image: 'assets/images/playstation4.png', active: false }
+]
+
 export default Ember.Route.extend({
+  model() {
+    return deals;
+  }
 });
