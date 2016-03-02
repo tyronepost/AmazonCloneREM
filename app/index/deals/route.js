6f9b90b5cd53ecc31f77d06ab6810c695e889727
Tyrone Post <tyrone.post.a@gmail.com>
2016-03-02 15:02:10 -0500

    adding wii u image to deals page.

--- app/index/deals/route.js
+++ app/index/deals/route.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 var deals = [
   { image: 'assets/images/xboxone.png', active: true },
-  { image: 'assets/images/playstation4.png', active: false }
+  { image: 'assets/images/playstation4.png', active: false },
+  { image: 'assets/images/wiiu.jpg', active: false }
 ];
 
 export default Ember.Route.extend({
