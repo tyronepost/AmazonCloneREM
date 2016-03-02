3ae73c31d6e2caf6f230978d6f3f8e772cd12dc2
Tyrone Post <tyrone.post.a@gmail.com>
2016-03-02 14:54:47 -0500

    fixed missing semicolon in /index/deals/route.js

--- app/index/deals/route.js
+++ app/index/deals/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 var deals = [
   { image: 'assets/images/xboxone.png', active: true },
   { image: 'assets/images/playstation4.png', active: false }
-]
+];
 
 export default Ember.Route.extend({
   model() {
