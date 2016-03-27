45887d88c3454799c3721b4d21af57b6d1acff6d
Tyrone Post <tyrone.post.a@gmail.com>
2016-03-27 18:22:12 -0400

    adding unit test for deals route.

--- tests/unit/index/deals/route-test.js
+++ tests/unit/index/deals/route-test.js
@@ -8,4 +8,15 @@ moduleFor('route:index/deals', 'Unit | Route | index/deals', {
 test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
+
+  var deals = route.model();
+  assert.ok(deals);
+
+  var xbox = deals[0];
+  var ps4 = deals[1];
+  var wiiu = deals[2];
+
+  assert.ok(xbox.name === 'Xbox One');
+  assert.ok(ps4.name === 'Playstation 4');
+  assert.ok(wiiu.name === 'Wii U');
 });
